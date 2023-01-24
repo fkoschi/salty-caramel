f284323034a6e7f46be48cc4531a37db15abe1f3
Felix Koschmidder <felix.koschi@gmail.com>
2023-01-24 22:45:56 +0100

    chore: root build script for admin

--- packages/admin/src/pages/a.jsx
+++ packages/admin/src/pages/a.jsx
@@ -20,6 +20,7 @@ import ProductsRoute from "../domain/products"
 import SalesChannels from "../domain/sales-channels"
 import Settings from "../domain/settings"
 import PublishableApiKeys from "../domain/publishable-api-keys"
+import { QueryClient, QueryClientProvider } from "react-query"
 
 const IndexPage = () => {
   const navigate = useNavigate()
@@ -34,31 +35,35 @@ const IndexPage = () => {
 }
 
 const DashboardRoutes = () => {
+  const queryClient = new QueryClient()
+
   return (
-    <AnalyticsProvider writeKey={WRITE_KEY}>
-      <DndProvider backend={HTML5Backend}>
-        <Layout>
-          <SEO title="Medusa" />
-          <Routes className="h-full">
-            <Route path="oauth/:app_name" element={<Oauth />} />
-            <Route path="products/*" element={<ProductsRoute />} />
-            <Route path="collections/*" element={<Collections />} />
-            <Route path="gift-cards/*" element={<GiftCards />} />
-            <Route path="orders/*" element={<Orders />} />
-            <Route path="draft-orders/*" element={<DraftOrders />} />
-            <Route path="discounts/*" element={<Discounts />} />
-            <Route path="customers/*" element={<Customers />} />
-            <Route path="pricing/*" element={<Pricing />} />
-            <Route path="settings/*" element={<Settings />} />
-            <Route path="sales-channels/*" element={<SalesChannels />} />
-            <Route
-              path="publishable-api-keys/*"
-              element={<PublishableApiKeys />}
-            />
-          </Routes>
-        </Layout>
-      </DndProvider>
-    </AnalyticsProvider>
+    <QueryClientProvider client={queryClient}>
+      <AnalyticsProvider writeKey={WRITE_KEY}>
+        <DndProvider backend={HTML5Backend}>
+          <Layout>
+            <SEO title="Medusa" />
+            <Routes className="h-full">
+              <Route path="oauth/:app_name" element={<Oauth />} />
+              <Route path="products/*" element={<ProductsRoute />} />
+              <Route path="collections/*" element={<Collections />} />
+              <Route path="gift-cards/*" element={<GiftCards />} />
+              <Route path="orders/*" element={<Orders />} />
+              <Route path="draft-orders/*" element={<DraftOrders />} />
+              <Route path="discounts/*" element={<Discounts />} />
+              <Route path="customers/*" element={<Customers />} />
+              <Route path="pricing/*" element={<Pricing />} />
+              <Route path="settings/*" element={<Settings />} />
+              <Route path="sales-channels/*" element={<SalesChannels />} />
+              <Route
+                path="publishable-api-keys/*"
+                element={<PublishableApiKeys />}
+              />
+            </Routes>
+          </Layout>
+        </DndProvider>
+      </AnalyticsProvider>
+    </QueryClientProvider>
   )
 }
 
