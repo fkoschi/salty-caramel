0f2b82de90b50707c4e4dd93d9f207939aed5f5a
Felix Koschmidder <felix.koschi@gmail.com>
2023-01-30 22:43:01 +0100

    feat(storefront): queryclientprovider

--- packages/storefront/src/pages/_app.tsx
+++ packages/storefront/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AccountProvider } from "@lib/context/account-context"
 import { CartDropdownProvider } from "@lib/context/cart-dropdown-context"
 import { MobileMenuProvider } from "@lib/context/mobile-menu-context"
 import { StoreProvider } from "@lib/context/store-context"
+import { QueryClientProvider } from 'react-query';
 import { CartProvider, MedusaProvider } from "medusa-react"
 import { Hydrate } from "react-query"
 import "styles/globals.css"
@@ -15,26 +16,28 @@ function App({
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return (
-    <MedusaProvider
-      baseUrl={MEDUSA_BACKEND_URL}
-      queryClientProviderProps={{
-        client: queryClient,
-      }}
-    >
-      <Hydrate state={pageProps.dehydratedState}>
-        <CartDropdownProvider>
-          <MobileMenuProvider>
-            <CartProvider>
-              <StoreProvider>
-                <AccountProvider>
-                  {getLayout(<Component {...pageProps} />)}
-                </AccountProvider>
-              </StoreProvider>
-            </CartProvider>
-          </MobileMenuProvider>
-        </CartDropdownProvider>
-      </Hydrate>
-    </MedusaProvider>
+    <QueryClientProvider client={queryClient}>
+      <MedusaProvider
+        baseUrl={MEDUSA_BACKEND_URL}
+        queryClientProviderProps={{
+          client: queryClient,
+        }}
+      >
+        <Hydrate state={pageProps.dehydratedState}>
+          <CartDropdownProvider>
+            <MobileMenuProvider>
+              <CartProvider>
+                <StoreProvider>
+                  <AccountProvider>
+                    {getLayout(<Component {...pageProps} />)}
+                  </AccountProvider>
+                </StoreProvider>
+              </CartProvider>
+            </MobileMenuProvider>
+          </CartDropdownProvider>
+        </Hydrate>
+      </MedusaProvider>
+    </QueryClientProvider>
   )
 }
 
